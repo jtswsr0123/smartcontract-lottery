fa4f56ace257b23bf39d628a8e070df795782443
zhojoe <zyj2000@gmail.com>
2022-05-02 21:43:59 +0800

    brownie_lottery_test

---
+++ README.md
@@ -0,0 +1,9 @@
+1. users can enter lottery with ETH based on a USD fee
+2. An admin will choose when the lottery is over
+3. the lottery will select a random winner
+
+Testing:
+
+1. mainnet-fork
+2. developement with mocks
+3. testnet
---
+++ tests/test_lottery_integration.py
@@ -0,0 +1,26 @@
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_account,
+    fund_with_link,
+)
+import time
+import pytest
+from brownie import network
+
+def test_can_pick_winner(check_local_blockchain_envs, lottery_contract):
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+
+    account = get_account()
+    lottery_contract.startLottery({"from": account})
+    lottery_contract.enter(
+        {"from": account, "value": lottery_contract.getEntranceFee()}
+    )
+    lottery_contract.enter(
+        {"from": account, "value": lottery_contract.getEntranceFee()}
+    )
+    fund_with_link(lottery_contract)
+    lottery_contract.endLottery({"from": account})
+    time.sleep(180)
+    assert lottery_contract.recentWinner() == account
+    assert lottery_contract.balance() == 0
